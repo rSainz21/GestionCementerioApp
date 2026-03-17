bdf9b81bf681f06aaa41013b90b8e9b4f5a2db62
manu-m <mmediavillaa02@educantabria.es>
2026-03-17 11:26:56 +0100

    creado api

---
+++ src/main/java/com/example/cementerio_api/service/RestosService.java
@@ -0,0 +1,12 @@
+package com.example.cementerio_api.service;
+
+
+
+import com.example.cementerio_api.entity.CemenRestos;
+import java.util.List;
+
+public interface RestosService {
+    List<CemenRestos> listarHuerfanos();
+    CemenRestos vincularANicho(Integer restoId, Integer unidadId);
+    List<CemenRestos> buscarPorNombre(String nombre);
+}
