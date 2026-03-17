03e6af10f1e550b23555acd43b8e0ae94e4a8bd2
manu-m <mmediavillaa02@educantabria.es>
2026-03-17 10:44:52 +0100

    creado api

---
+++ src/test/java/com/example/cementerio_api/CementerioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cementerio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CementerioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
